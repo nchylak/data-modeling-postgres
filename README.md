5842f47a53260dfefa48e8ae6252beb536e7f0a4
Nadia Chylak <nadia.chylak@gmail.com>
2021-09-23 10:36:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Song Play ETL Project
+
+## Summary
+
+This project consists in reading song data from a subset of data from the [Million Song Dataset](https://labrosa.ee.columbia.edu/millionsong/) as well as user activity logs from a music streaming app and combine these for analytics purposes in a PostgreSQL database in the following star schema:
+
+##### Fact Table
+
+1. songplays
+   * records in log data associated with song plays i.e. records with page `NextSong`
+     * *songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent*
+
+##### Dimension Tables
+
+1. users
+   * users in the app
+     * *user_id, first_name, last_name, gender, level*
+2. songs
+   * songs in music database
+     * *song_id, title, artist_id, year, duration*
+3. artists
+   * artists in music database
+     * *artist_id, name, location, latitude, longitude*
+4. times
+   - timestamps of records in **songplays** broken down into specific units
+     - *start_time, hour, day, week, month, year, weekday*
+
+## Files description
+
+1. `create_tables.py` drops and creates the relevant tables in the database. This script is used to reset tables before running the ETL script.
+2. `sql_queries.py` contains all SQL queries used in this project.
+3. `etl.ipynb` reads and processes a single file from `data/song_data` and `data/log_data` and loads the data into the tables. This notebook was meant for iteratively writing and testing code before implementing its final version into `etl.py`.
+4. `etl.py` reads and processes files from `data/song_data` and `data/log_data` and loads them into the tables.
+5. `test.ipynb` displays the first few rows of each table for testing purposes.
+
+## How to run this project
+
+```bash
+python create_tables.py
+python etl.py
+```
+
+After the scripts have run, you can execute `test.ipynb` for testing purposes.
---
+++ sql_queries.py
@@ -0,0 +1,127 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS times;"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id SERIAL PRIMARY KEY,
+        start_time timestamp,
+        user_id varchar,
+        level varchar,
+        song_id varchar,
+        artist_id varchar,
+        session_id varchar,
+        location varchar,
+        user_agent varchar
+        );
+""")
+
+user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id varchar PRIMARY KEY,
+        first_name varchar,
+        last_name varchar,
+        gender varchar,
+        level varchar
+        );
+""")
+
+song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id varchar PRIMARY KEY,
+        title varchar,
+        artist_id varchar,
+        year int,
+        duration float
+        );
+""")
+
+artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id varchar PRIMARY KEY,
+        name varchar,
+        location varchar,
+        latitude float,
+        longitude float
+        );
+""")
+
+time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS times (
+        start_time timestamp PRIMARY KEY,
+        hour int,
+        day int,
+        week int,
+        month int,
+        year int,
+        weekday int
+        );
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+    INSERT INTO songplays
+    VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (songplay_id)
+    DO NOTHING;
+""")
+
+user_table_insert = ("""
+    INSERT INTO users
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id)
+    DO UPDATE
+        SET 
+            first_name  = EXCLUDED.first_name,
+            last_name = EXCLUDED.last_name,
+            gender  = EXCLUDED.gender,
+            level = EXCLUDED.level
+    ;
+""")
+
+song_table_insert = ("""
+    INSERT INTO songs
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id)
+    DO NOTHING;
+""")
+
+artist_table_insert = ("""
+    INSERT INTO artists
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id)
+    DO NOTHING;
+""")
+
+time_table_insert = ("""
+    INSERT INTO times
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time)
+    DO NOTHING;
+""")
+
+# FIND SONGS
+
+song_select = ("""
+    SELECT
+        songs.song_id,
+        songs.artist_id
+    FROM songs
+    LEFT JOIN artists ON artists.artist_id = songs.artist_id
+    WHERE
+        songs.title = %s
+        AND artists.name = %s
+        AND ABS(songs.duration - %s) < 5;       
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
